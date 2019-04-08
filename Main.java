5effcdbef3caf064f66f38cd4d401167306ad4e9
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-04-08 13:29:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,11 +19,11 @@ public class Conn {
 
 	 	///Funcion para conectar
 	public Connection getConnection () {
-		Connection conn = null;
+		Connection conexionPrimaria = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.driver");
-			conn = DriverManager.getConnection(URL,user,pass);
+			conexionPrimaria = DriverManager.getConnection(URL,user,pass);
 			///Mesanje de exito;
 
 		} catch (Exception e) {
